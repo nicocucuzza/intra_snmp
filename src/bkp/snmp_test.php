2caee8631a870d3dfb6f4808c1cedd5d1629c54b
Nicolas Cucuzza <n.cucuzza@gmail.com>
2016-10-19 22:51:49 -0300

    v2.0

--- src/bkp/snmp_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$response = snmpget("localhost","public", ".1.3.6.1.2.1.1.2.0");
-print_r($response);
-
-?>
--- src/bkp/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$snmp = new SNMP(SNMP::VERSION_1, "localhost","public", 500);
-
-?>
